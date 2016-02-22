1f568d97fd8c03a1714aebc30825ccdc5288738e
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2016-02-22 22:34:53 +0530

    Bunch Updates from @mamun67
    This software downloads the data from Yahoo Finance and then downloads the data form the Yahoo Finance and saves in a .csv file in a continuous Integrative iteration.

--- src/marketshare/Marketshare.java
+++ src/marketshare/Marketshare.java
@@ -10,55 +10,118 @@ package marketshare;
  * @author Mamun Arfin, 4th Year(2016), Techno India College of Technology
  * @author Yajnavalkya Bandyopadhyay, 2nd Year(2016), Techno India College of Technology
  */
-import java.io.IOException;  
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;  
+import java.io.BufferedReader;
+//jsoup lib
 import org.jsoup.Jsoup;  
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 class Marketshare {
-static String date = (new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime())).toString();
-public static void main( String[] args ) throws IOException{  
-	
-	
-	//Document doc = Jsoup.parse(input);
-	int count;
+static String date = (new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime())).toString();
+    public static void main(String[] args) throws Exception {
+        
+               WebClient webClient = new WebClient(BrowserVersion.FIREFOX_38); //Initiate a WebClient variable.  
+               webClient = tremorLogin(webClient);
+                  GenFile(webClient);
+             
+        
+    }
+    //Login into Yahoo and return the client(Saves the cookies).
+      private static WebClient tremorLogin(WebClient webClient) throws Exception
+    {
+        webClient.getOptions().setJavaScriptEnabled(false); int netflag=1;   
         try{
-            
+        final HtmlPage currentPage = webClient.getPage("https://login.yahoo.com/");}
+        catch(Exception e){netflag=0;
+        System.out.println("TICT net failure");}//Load page at the STRING address.
+        if(netflag==1){
+        final HtmlPage currentPage = webClient.getPage("https://login.yahoo.com/");
+        final  HtmlInput username = currentPage.getElementByName("username"); //Find element called loginuser for username
+        username.setValueAttribute("nizamuddin_laskar@yahoo.com"); //Set value for username       
+        final  HtmlInput password = currentPage.getElementByName("passwd"); //Find element called loginpassword for password
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String uPwd = br.readLine();
+        password.setValueAttribute(uPwd); //Set value for password   
+        WebResponse response1 = currentPage.getWebResponse();
+        String content2 = response1.getContentAsString();    
+        final HtmlPage page2; 
+        HtmlElement htmlElement = currentPage.getFirstByXPath("//*[@id=\"login-signin\"]");
+        HtmlPage src=htmlElement.click();       
+        return webClient;
+        }
+    return webClient;
+    }
+      
+      private static void GenFile(WebClient webClient) throws IOException
+      {
+              try {
+        while (true) {
+            System.out.println(new Date());
+            Thread.sleep(1000);
+       
+          Page page = webClient.getPage("https://in.finance.yahoo.com/portfolio/p_6/view/v1");
+              
+          WebResponse response = page.getWebResponse();
+               String content = response.getContentAsString();
+              // System.out.println(content);
+          
             File file = new File("./output/"+date);
             if(file.isDirectory()){
-            csvcreate();
+             createcsv(content);
             }else{File outp = new File("./output");
                 if(outp.isDirectory()){
                     File dir = new File("./output/"+date);
                     dir.mkdir();
-                    csvcreate();
+                    createcsv(content);
                 }
                 else{
                     outp.mkdir();
                     File dir = new File("./output/"+date);
                     dir.mkdir();
-                    csvcreate();
+                     createcsv(content);
                 }
             }
-        }
-        catch(Exception e){}
-        //    String title = doc.title();  
-        //    System.out.println("title is: " + title);  
-}
-public static void csvcreate() throws IOException{
-        File input = new File("web.html");
-	Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+            
+             }
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+           
+      }
+      
+      public static void createcsv(String content) throws IOException
+      {
+          
+          
+          
+         // the code for csv file creation goes here... 
+          //File input = new File("web.html");
+         // File input = (File) content;
+	//Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+          Document doc = Jsoup.parse(content);
         File file = new File("./output/"+date);
         String fname = Integer.toString(file.listFiles().length +1);
-    try {
-		FileWriter writer = new FileWriter("./output/"+date+"/"+fname+".csv");
-	
+          
+         
+	//Document doc = Jsoup.parse(input);
+	try {
+		//FileWriter writer = new FileWriter("dataout.csv");
+                FileWriter writer = new FileWriter("./output/"+date+"/stockdata_"+fname+".csv");
 		for (Element table : doc.select("table.yfi_portfolios_multiquote")) {
 			for (Element row : table.select("tr")) {
 				Elements ths = row.select("th");
@@ -78,6 +141,7 @@ public static void csvcreate() throws IOException{
 	} catch (IOException e) {
 		e.getStackTrace();
 	}
+          
+          
+      }
 }
-}
-
