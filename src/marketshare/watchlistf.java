3d35ad734232d3cb86f632d00049990cd5cdbdcf
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2016-05-09 13:47:18 +0530

    Value fetcher from Rediff Watchlist
    *Watchlistf.java : It will be coded to scrap the codes from the rediff watchlist page
     which accepts the webclient from the main class afer login as parameter and uses to scrap
    
    *.gitignore : rediff.html is a file being created for debugging purpose which is being blacklisted for git add using .gitignore

---
+++ src/marketshare/watchlistf.java
@@ -0,0 +1,25 @@
+        /*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marketshare;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author tict
+ */
+public class watchlistf {
+    public void wfetcher(WebClient wc) throws IOException{
+        String link = "http://portfolio.rediff.com/watchlist?src=top_nav";
+        Page page=  wc.getPage(link);
+        WebResponse ws =page.getWebResponse();
+        String content = page.getWebResponse().getContentAsString();        
+    }
+}
